2b58f2502e18d3d1d4e02ef76f22001c4718b61a
shen.xing@zyxr.com <shen.xing@zyxr.com>
2017-04-12 20:10:44 +0800

    swipe to refresh

---
+++ app/src/main/java/com/study/shenxing/caesar/swipetorefresh/SwipeToRefreshActivity.java
@@ -0,0 +1,15 @@
+package com.study.shenxing.caesar.swipetorefresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.study.shenxing.caesar.R;
+
+public class SwipeToRefreshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_to_refresh);
+    }
+}
