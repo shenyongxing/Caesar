ff2d4b43addd9867844bfed7a5e892dccb9a23f2
shenxing <shenxing583@gmail.com>
2017-10-17 10:49:33 +0800

    常规提交

---
+++ app/src/main/java/com/study/shenxing/caesar/customview/RingView.java
@@ -0,0 +1,14 @@
+package com.study.shenxing.caesar.customview;
+
+import android.view.ViewGroup;
+
+/**
+ * Copyright © 2017年 深圳中业兴融互联网金融服务有限公司. All rights reserved.
+ *
+ * @Author shenxing
+ * @Date 2017/9/30
+ * @Email shen.xing@zyxr.com
+ * @Description
+ */
+public class RingView extends ViewGroup {
+}
